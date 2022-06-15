dd844294274a1ffce3ebb034f01438bb4193a1f0
zach <zachanon@gmail.com>
2022-06-15 11:29:43 -0600

    more traits

--- math/src/traits.rs
+++ math/src/traits.rs
@@ -1,6 +1,8 @@
 use anchor_lang::{error, error_code, prelude::Result};
 use num_traits::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub};
 
+use crate::{Number, U192};
+
 #[error_code]
 pub enum NumericalError {
     #[msg("overflow on checked add")]
@@ -32,6 +34,46 @@ pub trait TrySubAssign: CheckedSub {
 }
 impl<T: CheckedAdd> TryAddAssign for T {}
 impl<T: CheckedSub> TrySubAssign for T {}
+
+pub trait ToNumber {
+    fn to_number(self) -> Number;
+}
+
+impl<T: Into<U192>> ToNumber for T {
+    fn to_number(self) -> Number {
+        self.into().into()
+    }
+}
+
+pub trait NumberAddAssign {
+    fn try_number_add_assign(&mut self, amount: impl Into<Number>) -> Result<()>;
+}
+
+impl NumberAddAssign for [u8; 24] {
+    fn try_number_add_assign(&mut self, amount: impl Into<Number>) -> Result<()> {
+        *self = Number::from_bits(*self)
+            .checked_add(&amount.into())
+            .ok_or_else(|| error!(NumericalError::AdditionOverflow))?
+            .into();
+
+        Ok(())
+    }
+}
+
+pub trait NumberSubAssign {
+    fn try_number_sub_assign(&mut self, amount: impl Into<Number>) -> Result<()>;
+}
+
+impl NumberSubAssign for [u8; 24] {
+    fn try_number_sub_assign(&mut self, amount: impl Into<Number>) -> Result<()> {
+        *self = Number::from_bits(*self)
+            .checked_sub(&amount.into())
+            .ok_or_else(|| error!(NumericalError::SubtractionUnderflow))?
+            .into();
+
+        Ok(())
+    }
+}
 
 pub trait SafeAdd: CheckedAdd {
     fn safe_add(&self, amount: Self) -> Result<Self> {
