e19e8ab567d6f8dc0fbf753358ae97461ff032bb
zachanon <zachanon@gmail.com>
2022-09-01 01:11:10 -0600

    fp math

--- math/src/fixed_point.rs
+++ math/src/fixed_point.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
 use bytemuck::{Pod, Zeroable};
 
@@ -15,21 +15,17 @@ impl Fp32 {
     pub const MIN: Self = Self(u128::MIN);
 
     pub fn as_u64(self) -> Option<u64> {
-        static BOUND: u128 = u64::MAX as u128;
-        if self.0 > BOUND {
+        let res = self.0 / Self::ONE.0;
+        if res > u64::MAX as u128 {
             None
         } else {
-            Some(self.0 as u64)
+            Some(res as u64)
         }
     }
 
-    pub fn as_u64_ciel(&self) -> Option<u64> {
+    pub fn as_u64_ciel(self) -> Option<u64> {
         let add_one = (!(self.0 as u32)).wrapping_add(1) as u128;
-        self.0.checked_add(add_one).map(|n| n as u64)
-    }
-
-    pub fn as_u64_floor(&self) -> u64 {
-        (self.0 >> 32) as u64
+        self.0.checked_add(add_one).and_then(|n| Self(n).as_u64())
     }
 }
 
@@ -55,6 +51,25 @@ impl Sub<Fp32> for Fp32 {
     }
 }
 
+impl AddAssign<Fp32> for Fp32 {
+    fn add_assign(&mut self, rhs: Fp32) {
+        self.0.add_assign(rhs.0)
+    }
+}
+
+impl SubAssign<Fp32> for Fp32 {
+    fn sub_assign(&mut self, rhs: Fp32) {
+        self.0.sub_assign(rhs.0)
+    }
+}
+
+impl MulAssign<Fp32> for Fp32 {
+    fn mul_assign(&mut self, rhs: Fp32) {
+        self.0.mul_assign(rhs.0);
+        self.0.div_assign(Self::ONE.0);
+    }
+}
+
 impl Mul<Fp32> for Fp32 {
     type Output = Fp32;
 
@@ -68,6 +83,22 @@ impl Div<Fp32> for Fp32 {
 
     fn div(self, rhs: Fp32) -> Self::Output {
         Self(self.0.mul(FP32_ONE).div(rhs.0))
+    }
+}
+
+impl<T: Into<u128>> Mul<T> for Fp32 {
+    type Output = Fp32;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self(self.0.mul(rhs.into()))
+    }
+}
+
+impl<T: Into<u128>> Div<T> for Fp32 {
+    type Output = Fp32;
+
+    fn div(self, rhs: T) -> Self::Output {
+        Self(self.0.div(rhs.into()))
     }
 }
 
@@ -106,4 +137,96 @@ mod tests {
         assert_eq!(Fp32::ONE, Fp32::ONE / Fp32::ONE);
         assert_eq!(Fp32::ONE, Fp32::from(1u64) / Fp32::ONE);
     }
+
+    #[test]
+    fn one_thousand_div_100_equals_ten() {
+        assert_eq!(Fp32::from(10u64), Fp32::from(1_000u64) / Fp32::from(100u64));
+    }
+
+    #[test]
+    fn multiply_by_u64() {
+        assert_eq!(Fp32::from(3u64), Fp32::from(1u64) * 3u64)
+    }
+
+    #[test]
+    fn test_add_assign_101_2() {
+        let mut a = Fp32::from(101u64);
+        a += Fp32::from(2u64);
+        assert_eq!(Fp32::from(103u64), a);
+    }
+
+    #[test]
+    fn test_sub_assign_101_2() {
+        let mut a = Fp32::from(101u64);
+        a -= Fp32::from(2u64);
+        assert_eq!(Fp32::from(99u64), a);
+    }
+
+    #[test]
+    fn test_mul_assign_101_2() {
+        let mut a = Fp32::from(101u64);
+        a *= Fp32::from(2u64);
+        assert_eq!(Fp32::from(202u64), a);
+    }
+
+    #[test]
+    fn test_div_assign_101_2() {
+        let mut a = Fp32::from(101u64);
+        a = a / Fp32::from(2u64);
+        assert_eq!(Fp32::from(505u64) / Fp32::from(10u64), a);
+    }
+
+    #[test]
+    fn as_u64() {
+        let u64in = 31455u64;
+        let a = Fp32::from(u64in);
+        let b = a.as_u64().unwrap();
+        assert_eq!(b, u64in);
+    }
+
+    #[test]
+    #[should_panic]
+    fn as_u64_panic_big() {
+        let a = Fp32::from(u64::MAX as u128 + 1);
+        a.as_u64().unwrap();
+    }
+
+    // #[test]
+    // fn as_f64() {
+    //     let n = Number128::from_bps(15000);
+    //     assert_eq!(1.5, n.as_f64());
+
+    //     // Test that conversion is within bounds and doesn't lose precision for min
+    //     let n = Number128::MIN; // -170141183460469231731687303715884105728
+    //     assert_eq!(-17014118346046923173168730371.5884105728, n.as_f64());
+
+    //     // Test that conversion is within bounds and doesn't lose precision for max
+    //     let n = Number128::MAX; // 170141183460469231731687303715884105727
+    //     assert_eq!(17014118346046923173168730371.5884105727, n.as_f64());
+
+    //     // More cases
+    //     let n = Number128::from_bps(0) - Number128::from_bps(15000);
+    //     assert_eq!(-1.5, n.as_f64());
+
+    //     let n = Number128::from_decimal(12345678901i128, -10);
+    //     assert_eq!(1.2345678901, n.as_f64());
+
+    //     let n = Number128::from_decimal(-12345678901i128, -10);
+    //     assert_eq!(-1.2345678901, n.as_f64());
+
+    //     let n = Number128::from_decimal(-12345678901i128, -9);
+    //     assert_eq!(-12.345678901, n.as_f64());
+
+    //     let n = Number128::from_decimal(12345678901i128, -9);
+    //     assert_eq!(12.345678901, n.as_f64());
+
+    //     let n = Number128::from_decimal(ONE - 1, 1);
+    //     assert_eq!(99999999990.0, n.as_f64());
+
+    //     let n = Number128::from_decimal(12345678901i128, -13);
+    //     assert_eq!(0.0012345678, n.as_f64());
+
+    //     let n = Number128::from_decimal(-12345678901i128, -13);
+    //     assert_eq!(-0.0012345678, n.as_f64());
+    // }
 }
