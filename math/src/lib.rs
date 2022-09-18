288fa2c7b077c14d5134848c0ff0f42ef77f0434
zachanon <zachanon@gmail.com>
2022-09-18 22:12:22 +0700

    modules

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,24 +1,12 @@
-#[cfg(not(feature = "fixed-point"))]
-mod functions;
-#[cfg(not(feature = "fixed-point"))]
-mod number;
-#[cfg(not(feature = "fixed-point"))]
-mod number_128;
+#[cfg(feature = "number")]
+pub mod functions;
+#[cfg(feature = "number")]
+pub mod number;
+#[cfg(feature = "number-128")]
+pub mod number_128;
 
 #[cfg(feature = "traits")]
 pub mod traits;
 
 #[cfg(feature = "fixed-point")]
 pub mod fixed_point;
-
-#[cfg(not(feature = "fixed-point"))]
-#[doc(inline)]
-pub use functions::*;
-
-#[cfg(not(feature = "fixed-point"))]
-#[doc(inline)]
-pub use number::*;
-
-#[cfg(not(feature = "fixed-point"))]
-#[doc(inline)]
-pub use number_128::*;
